0a0924e104b63febe36b64f3292926d934d3acea
Yogi Pratama <yoogikpratama@gmail.com>
2023-11-29 22:44:30 +0700

    Add absolute path to open file

--- web/handler/wadek_dashboard.go
+++ web/handler/wadek_dashboard.go
@@ -117,8 +117,8 @@ func (h *wadekDashboardHandler) Update(c *gin.Context) {
 	}
 
 	input.ID = id
-
-	file, err := os.ReadFile("/file_document/" + documentLegalization.FileNameDocument)
+	pwd, _ := os.Getwd()
+	file, err := os.ReadFile(pwd + "/file_document/" + documentLegalization.FileNameDocument)
 	if err != nil {
 		log.Printf("Error : %v", err)
 		c.HTML(http.StatusInternalServerError, "error.html", nil)
@@ -137,7 +137,7 @@ func (h *wadekDashboardHandler) Update(c *gin.Context) {
 		wg.Done()
 	}()
 	wg.Wait()
-	message, err := os.ReadFile("/file_signed/" + documentLegalization.FileNameDocument)
+	message, err := os.ReadFile(pwd + "/file_signed/" + documentLegalization.FileNameDocument)
 	if err != nil {
 		log.Printf("Error : %v", err)
 		c.HTML(http.StatusInternalServerError, "error.html", nil)
